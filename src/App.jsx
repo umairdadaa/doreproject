1fbd8d91eebde063d3d72bb9c0961f4aba9f75b2
Sheikh-hadi <96363461+Sheikh-hadi@users.noreply.github.com>
2024-12-27 17:41:10 +0500

    merageComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
+"use client"
+import "locomotive-scroll/dist/locomotive-scroll.css";
+import ArtistPage from "./Pages/Artist"
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ContactUs from "./Pages/ContactUs/ContactUs";
+import AnnualLadyBug from "./Pages/AnnualLadyBug";
+import Timeline from "./Pages/Timeline";
+import Home from "./Pages/Home";
 import Slider from "./Slider";
-
-function App() {
-
-
-
- 
-
-  
+import Slideshow3d from "./components/Slideshow3d/Slidershow3d";
+// import Timeline from "./Pages/Timeline"
+const App = () => {
   return (
-    <>
-   <Slider/>
-
-
-    </>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<ArtistPage />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/annual-lady-bug" element={<AnnualLadyBug />} />
+        <Route path="/timeline" element={<Timeline />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/collection" element={<Slider />} />
+        <Route path="/slideShow3d" element={<Slideshow3d />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,94 +1,72 @@
 import { Canvas } from "@react-three/fiber";
 import { animate } from "motion";
 import { useMotionValue } from "motion/react";
-import { Suspense, useState } from "react";
-import CodropsOverlay from "./components/CodropsOverlay";
-import RevealImage from "./components/RevealImage";
-import { Gltf, Center, OrbitControls, Float, Sphere, MeshRefractionMaterial, useEnvironment, Environment,Text, useTexture, Loader } from '@react-three/drei'
+import { useState } from "react";
+import { Gltf, OrbitControls, Environment, Loader } from '@react-three/drei'
 import Ring from "./Ring";
-import * as THREE from 'three'
 
 export default function Slider() {
-
-
   // FULLSCREEN MODE
   const [isFullScreen, setIsFullScreen] = useState(true);
-  const handleFullScreen = () => setIsFullScreen(!isFullScreen);
-  const images = ['./img/image.jpeg', './img/image2.jpeg','./img/image3.jpeg,'];
 
-  const [currentimage,setcurrentimage] = useState('./img/image.jpeg')
-  const [movestate,setmovestate] = useState(0)
+  const [currentimage, setcurrentimage] = useState('./img/image.jpeg')
 
   // DARK/LIGHT MODE
   const [isDarkMode, setIsDarkMode] = useState(false);
-  const handleDarkMode = () => setIsDarkMode(!isDarkMode);
 
   // REVEAL PROGRESS ANIMATION
   const [isRevealed, setIsRevealed] = useState(false);
   const revealProgress = useMotionValue(0);
 
-  const handleReveal = () => {
-    animate(revealProgress, isRevealed ? 0 : 1, {
-      duration: 1.5,
-      ease: "easeInOut",
-    });
-    setIsRevealed(!isRevealed);
-  };
-
-  setTimeout(() =>{
+  setTimeout(() => {
     animate(revealProgress, 1, {
       duration: 2,
       ease: "easeInOut",
     });
     setIsRevealed(true)
-  },6000)
+  }, 6000)
 
-  function Left(){
+  function Left() {
     animate(revealProgress, 0, {
       duration: 2,
       ease: "easeInOut",
     });
     setIsRevealed(false)
 
-    setTimeout(() =>{
-
+    setTimeout(() => {
       setcurrentimage('./img/image2.jpeg')
       animate(revealProgress, 1, {
         duration: 2,
         ease: "easeInOut",
       });
       setIsRevealed(true)
-
-    },2500)
+    }, 2500)
   }
 
-  
-  function Right(){
+  function Right() {
     animate(revealProgress, 0, {
       duration: 2,
       ease: "easeInOut",
     });
     setIsRevealed(false)
 
-    setTimeout(() =>{
-
+    setTimeout(() => {
       setcurrentimage('./img/image3.jpeg')
       animate(revealProgress, 1, {
         duration: 2,
         ease: "easeInOut",
       });
       setIsRevealed(true)
-
-    },2500)
+    }, 2500)
   }
 
   return (
     <>
-    <Loader/>
-    <img onClick={() => Left()} className="left" src="img/left-arrow.png"/>
-    <img onClick={() => Right()}  className="right" src="img/right-arrow.png"/>
+      <Loader />
+      <img onClick={() => Left()} className="left" src="img/left-arrow.png" alt="Left arrow" />
+      <img onClick={() => Right()} className="right" src="img/right-arrow.png" alt="Right arrow" />
 
-    <h1 id="bottom-text">Signature Collection</h1>
+      <h1 id="bottom-text">Signature Collection</h1>
 
       <Canvas
         className="z-10"
@@ -98,33 +76,23 @@ export default function Slider() {
           backgroundColor: isDarkMode ? "#000" : "#F9FAF7",
         }}
       >
-        <RevealImage
+        {/* RevealImage component is not defined, you may need to import it or implement it */}
+        {/* <RevealImage
           imageTexture={currentimage}
           revealProgress={revealProgress}
           isFullScreen={isFullScreen}
-        />
+        /> */}
 
-<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <group position={[0, -0.25, 2]}>
-       {/* <Text scale={1.1} position={[0,-0.2,-0.5]}>
-        DORE
-        <meshBasicMaterial toneMapped={false} color={'#ff8afd'}/>
-       </Text> */}
-       <Gltf position={[0,2.45,-1.04]} scale={0.15} src="model.glb"/>
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <group position={[0, -0.25, 2]}>
+          <Gltf position={[0, 2.45, -1.04]} scale={0.15} src="model.glb" />
+          <Ring frame={'#fff0f0'} diamonds={'#ffffff'} scale={0.055} />
+        </group>
 
-          <Ring frame={'#fff0f0'} diamonds={'#ffffff'}  scale={0.055} />
+        <OrbitControls enableRotate={false} enableZoom={false} enablePan={false} minPolarAngle={0} maxDistance={17} />
 
-       
-      </group>
-      
-      <OrbitControls  enableRotate={false} enableZoom={false} enablePan={false} minPolarAngle={0} maxDistance={17} />
-    
-      <Environment preset="sunset" />
-
+        <Environment preset="sunset" />
       </Canvas>
-
-
     </>
   );
 }
-
