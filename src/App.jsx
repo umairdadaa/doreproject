1ce65fd1ea362198248712faeb3c11392729bbb7
hamzayaqoob1509 <hamzayaqoob879@gmail.com>
2025-01-26 22:37:42 +0500

    Remove deprecated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
 'use client';
 import 'locomotive-scroll/dist/locomotive-scroll.css';
-import ArtistPage from './Pages/Artist';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import ContactUs from './Pages/ContactUs/ContactUs';
-import AnnualLadyBug from './Pages/AnnualLadyBug';
-import Timeline from './Pages/Timeline';
-import Home from './Pages/Home';
-import Slider from './Slider';
-import Slideshow3d from './components/Slideshow3d/Slidershow3d';
-import VideoBackgroundPage from './Pages/VideoBackground';
-import Header from './components/MenuPage';
-import Floral from './Pages/Floral';
-import CollectionPage from './Pages/CollectionPage';
 import HomePage from "./Pages/HomePage";
 
 const App = () => {
   return (
       <Router>
         <Routes>
-          <Route path='/artist' element={<ArtistPage />} />
-          <Route path='/menu' element={<Header />} />
-          <Route path='/contact-us' element={<ContactUs />} />
-          <Route path='/annual-lady-bug' element={<AnnualLadyBug />} />
-          <Route path='/timeline' element={<Timeline />} />
-          <Route path='/home' element={<Home />} />
-          {/* <Route path='/collection' element={<Slider />} /> */}
-          <Route path='/collection' element={<CollectionPage />} />
-          <Route path='/slideShow3d' element={<Slideshow3d />} />
-          <Route path='/floral' element={<Floral />} />
-          {/* <Route path='/' element={<VideoBackgroundPage />} /> */}
           <Route path='/' element={<HomePage />} />
         </Routes>
       </Router>
