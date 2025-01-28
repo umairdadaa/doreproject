0b6b7e71450d813d182be69d4548798cd33952ab
hamzayaqoob1509 <hamzayaqoob879@gmail.com>
2025-01-29 00:24:05 +0500

    Mobile responsiveness

--- src/Pages/CollectionPage/index.jsx
+++ src/Pages/CollectionPage/index.jsx
@@ -60,6 +60,8 @@ const CollectionPage = ({ show }) => {
         for (let i = 0; i < numItems; i++) {
             const angle = (i / numItems) * Math.PI * 2;
             const position = new Vector3(Math.cos(angle) * r, 0, Math.sin(angle) * r);
+            // const position = new Vector3(0,0,0);
+
             const directionToOrigin = position.clone().negate().normalize(); // Vector pointing to the origin
             const quaternion = new Quaternion().setFromUnitVectors(
                 new Vector3(0, 0, 1), // Default forward vector
@@ -97,12 +99,12 @@ const CollectionPage = ({ show }) => {
     return (
         <div className={`text-white w-screen h-screen absolute top-0 left-0 overflow-hidden ${show ? "visible" : "invisible"} z-50`} >
             <img src={`https://pub-c2bb244c4b2641f99eb92df5396cefa1.r2.dev/Scene 2 A.jpg?cacheBust=${Math.floor(Date.now() / 10000)}`} crossOrigin="anonymous" width={'1080'} height={1080} className="w-screen h-screen object-cover absolute top-0 left-0 z-20" ref={imgRef} />
-            <img onClick={handlePrev} src={`https://pub-c2bb244c4b2641f99eb92df5396cefa1.r2.dev/img/left-arrow.png?cacheBust=${Math.floor(Date.now() / 10000)}`} alt='prev' className="absolute left-1 top-[63%] -translate-y-1/2 w-28 cursor-pointer z-50 md:left-10 md:top-1/2" />
-            <img onClick={handleNext} src={`https://pub-c2bb244c4b2641f99eb92df5396cefa1.r2.dev/img/right-arrow.png?cacheBust=${Math.floor(Date.now() / 10000)}`} alt='next' className="absolute right-1 top-[63%] -translate-y-1/2 w-28 cursor-pointer z-5 md:right-10 md:top-1/2" />
+            <img onClick={handlePrev} src={`https://pub-c2bb244c4b2641f99eb92df5396cefa1.r2.dev/img/left-arrow.png?cacheBust=${Math.floor(Date.now() / 10000)}`} alt='prev' className="absolute left-1 top-[50%] -translate-y-1/2 w-28 cursor-pointer z-50 md:left-10 md:top-1/2" />
+            <img onClick={handleNext} src={`https://pub-c2bb244c4b2641f99eb92df5396cefa1.r2.dev/img/right-arrow.png?cacheBust=${Math.floor(Date.now() / 10000)}`} alt='next' className="absolute right-1 top-[50%] -translate-y-1/2 w-28 cursor-pointer z-5 md:right-10 md:top-1/2" />
             <div className="absolute top-10 left-1/2 -translate-x-1/2 font-sans font-bold text-3xl z-50 text-sargie">{products.length > 0 && products[activeItem].name}</div>
             <div className="absolute bottom-[10rem] left-1/2 -translate-x-1/2 z-50 w-full md:bottom-28 md:w-auto">
                 <div className="font-bold text-3xl text-center mb-2 text-sargie">{products.length > 0 && products[activeItem].price} AED</div>
-                <div className="font-bold text-justify text-1 max-w-[500px] w-full">{products.length > 0 && products[activeItem].uagb_excerpt}</div>
+                <div className="font-bold text-center text-1 max-w-[500px] w-full pl-2 pr-2">{products.length > 0 && products[activeItem].uagb_excerpt}</div>
             </div>
             <button
                 onClick={() => handlePreBooking(products[activeItem])}
@@ -162,67 +164,67 @@ const Models = ({ items, active, paused, scaleFactor, productName }) => {
                     switch (productName) {
                         case 'Flitzy':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 10 * scaleFactor : 10 * scaleFactor, window.innerWidth < 768 ? 10 * scaleFactor : 15 * scaleFactor, 1]} >
+                                <sprite ref={el => ref.current[idx] = el} {...item} position={[item.position.x, window.innerWidth < 768 ? item.position.y+5 : item.position.y, item.position.z]} scale={[window.innerWidth < 768 ? 10 * scaleFactor : 10 * scaleFactor, window.innerWidth < 768 ? 10 * scaleFactor : 15 * scaleFactor, 1]} >
                                     <spriteMaterial attach="material" map={FlitzyTexture} />
                                 </sprite>
                             );
                         case 'Rosella':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 15 * scaleFactor : 25 * scaleFactor, window.innerWidth < 768 ? 20 * scaleFactor : 30 * scaleFactor, 1]} >
+                                <sprite ref={el => ref.current[idx] = el} {...item} position={[item.position.x, window.innerWidth < 768 ? item.position.y+5 : item.position.y, item.position.z]} scale={[window.innerWidth < 768 ? 15 * scaleFactor : 25 * scaleFactor, window.innerWidth < 768 ? 20 * scaleFactor : 30 * scaleFactor, 1]} >
                                     <spriteMaterial attach="material" map={RosellaTexture} />
                                 </sprite>
                             );
                         case 'Orphée':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 20 * scaleFactor : 30 * scaleFactor, window.innerWidth < 768 ?  35 * scaleFactor : 45 * scaleFactor, 1]} >
+                                <sprite ref={el => ref.current[idx] = el} {...item} position={[item.position.x, window.innerWidth < 768 ? item.position.y+5 : item.position.y, item.position.z]} scale={[window.innerWidth < 768 ? 20 * scaleFactor : 30 * scaleFactor, window.innerWidth < 768 ?  35 * scaleFactor : 45 * scaleFactor, 1]} >
                                     <spriteMaterial attach="material" map={OrphéeTexture} />
                                 </sprite>
                             );
                         case 'Zeke':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, 1]} >
+                                <sprite ref={el => ref.current[idx] = el} {...item} position={[item.position.x, window.innerWidth < 768 ? item.position.y+5 : item.position.y, item.position.z]} scale={[window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, 1]} >
                                     <spriteMaterial attach="material" map={ZekeTexture} />
                                 </sprite>
                             );
                         case 'Calla Lilly':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 15 * scaleFactor : 20 * scaleFactor, window.innerWidth < 768 ? 25 * scaleFactor : 25 * scaleFactor, 1]} >
+                                <sprite ref={el => ref.current[idx] = el} {...item} position={[item.position.x, window.innerWidth < 768 ? item.position.y+5 : item.position.y, item.position.z]} scale={[window.innerWidth < 768 ? 15 * scaleFactor : 20 * scaleFactor, window.innerWidth < 768 ? 25 * scaleFactor : 25 * scaleFactor, 1]} >
                                     <spriteMaterial attach="material" map={CallaLillyTexture} />
                                 </sprite>
                             );
                         case 'Bella':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, 1]} >
+                                <sprite ref={el => ref.current[idx] = el} {...item} position={[item.position.x, window.innerWidth < 768 ? item.position.y+5 : item.position.y, item.position.z]} scale={[window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, 1]} >
                                     <spriteMaterial attach="material" map={BellaTexture} />
                                 </sprite>
                             );
                         case 'Buzzy':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ?  40 * scaleFactor : 60 * scaleFactor, window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, 1]} >
+                                <sprite ref={el => ref.current[idx] = el} {...item} position={[item.position.x, window.innerWidth < 768 ? item.position.y+5 : item.position.y, item.position.z]} position={[item.position.x, window.innerWidth < 768 ? item.position.y+5 : item.position.y, item.position.z]} scale={[window.innerWidth < 768 ?  40 * scaleFactor : 60 * scaleFactor, window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, 1]} >
                                     <spriteMaterial attach="material" map={BuzzyTexture} />
                                 </sprite>
                             );
                         case 'Orabella':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ?  20 * scaleFactor : 25 * scaleFactor, window.innerWidth < 768 ? 25 * scaleFactor : 35 * scaleFactor, 1]} >
+                                <sprite ref={el => ref.current[idx] = el} {...item} position={[item.position.x, window.innerWidth < 768 ? item.position.y+5 : item.position.y, item.position.z]} scale={[window.innerWidth < 768 ?  20 * scaleFactor : 25 * scaleFactor, window.innerWidth < 768 ? 25 * scaleFactor : 35 * scaleFactor, 1]} >
                                     <spriteMaterial attach="material" map={OrabellaTexture} />
                                 </sprite>
                             );
                         case 'Aurélia':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 20 * scaleFactor : 30 * scaleFactor, window.innerWidth < 768 ? 30 * scaleFactor : 45 * scaleFactor, 1]} >
+                                <sprite ref={el => ref.current[idx] = el} {...item} position={[item.position.x, window.innerWidth < 768 ? item.position.y+5 : item.position.y, item.position.z]} scale={[window.innerWidth < 768 ? 20 * scaleFactor : 30 * scaleFactor, window.innerWidth < 768 ? 30 * scaleFactor : 45 * scaleFactor, 1]} >
                                     <spriteMaterial attach="material" map={AuréliaTexture} />
                                 </sprite>
                             );
                         case 'Améthia':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 15 * scaleFactor : 20 * scaleFactor, window.innerWidth < 768 ? 25 * scaleFactor : 25 * scaleFactor, 1]} >
+                                <sprite ref={el => ref.current[idx] = el} {...item} position={[item.position.x, window.innerWidth < 768 ? item.position.y+5 : item.position.y, item.position.z]} scale={[window.innerWidth < 768 ? 15 * scaleFactor : 20 * scaleFactor, window.innerWidth < 768 ? 25 * scaleFactor : 25 * scaleFactor, 1]} >
                                     <spriteMaterial attach="material" map={AméthiaTexture} />
                                 </sprite>
                             );
                         case 'Ziggy':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, 1]} >
+                                <sprite ref={el => ref.current[idx] = el} {...item} position={[item.position.x, window.innerWidth < 768 ? item.position.y+5 : item.position.y, item.position.z]} scale={[window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, 1]} >
                                     <spriteMaterial attach="material" map={ZiggyTexture} />
                                 </sprite>
                             );
