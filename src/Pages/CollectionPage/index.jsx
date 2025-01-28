bcf5130b90920d5b7224d14e494b5b2041a4bcca
hamzayaqoob1509 <hamzayaqoob879@gmail.com>
2025-01-28 17:03:56 +0500

    Set responsiveness of Collections

--- src/Pages/CollectionPage/index.jsx
+++ src/Pages/CollectionPage/index.jsx
@@ -8,6 +8,18 @@ import flowerImg from '../../../public/assets/Zeke.png';  // Local import for fl
 import ringImg from '../../../public/assets/Zeke.png';  // Local import for ring image
 import { lerp } from "three/src/math/MathUtils";
 
+import Améthia from '../../../public/assets/Améthia.png';  // Local import for ring image
+import Aurélia from '../../../public/assets/Aurélia.png';  // Local import for ring image
+import Bella from '../../../public/assets/Bella.png';  // Local import for ring image
+import Buzzy from '../../../public/assets/Buzzy.png';  // Local import for ring image
+import CallaLilly from '../../../public/assets/Calla Lilly.png';  // Local import for ring image
+import Flitzy from '../../../public/assets/Flitzy.png';  // Local import for ring image
+import Orabella from '../../../public/assets/Orabella.png';  // Local import for ring image
+import Orphée from '../../../public/assets/Orphée.png';  // Local import for ring image
+import Rosella from '../../../public/assets/Rosella.png';  // Local import for ring image
+import Zeke from '../../../public/assets/Zeke.png';  // Local import for ring image
+import Ziggy from '../../../public/assets/Ziggy.png';  // Local import for ring image
+
 const CollectionPage = ({ show }) => {
     const imgRef = useRef();
     const [products, setProducts] = useState([]);
@@ -66,17 +78,17 @@ const CollectionPage = ({ show }) => {
     useEffect(() => {
         setTimeout(() => { imgRef.current.style.opacity = 0; }, 500);
     }, []);
-
+    
     const handlePreBooking = (product) => {
-        if (!product || !product.name) {
-            console.error("Product name is required for pre-booking.");
+        console.log("Pre-booking clicked for product:", product);
+        if (!product || !product.id) {
+            console.error("Product ID is required for pre-booking.");
             return;
         }
     
-        const url = `https://shop.worldofdore.com/?product_name=${encodeURIComponent(product.name)}&price=${product.price}&quantity=1`;
-        window.location.href = url;
+        const url = product.permalink;
+        window.location.href = url; 
     };
-    
 
     const toggleAnimation = () => {
         setPaused(prev => !prev);  // Toggle the paused state
@@ -85,11 +97,13 @@ const CollectionPage = ({ show }) => {
     return (
         <div className={`text-white w-screen h-screen absolute top-0 left-0 overflow-hidden ${show ? "visible" : "invisible"} z-50`} >
             <img src="https://pub-c2bb244c4b2641f99eb92df5396cefa1.r2.dev/Scene 2 A.jpg" crossOrigin="anonymous" width={'1080'} height={1080} className="w-screen h-screen object-cover absolute top-0 left-0 z-20" ref={imgRef} />
-            <img onClick={handlePrev} src='https://pub-c2bb244c4b2641f99eb92df5396cefa1.r2.dev/img/left-arrow.png' alt='prev' className="absolute left-10 top-1/2 -translate-y-1/2 w-28 cursor-pointer z-50" />
-            <img onClick={handleNext} src='https://pub-c2bb244c4b2641f99eb92df5396cefa1.r2.dev/img/right-arrow.png' alt='next' className="absolute right-10 top-1/2 -translate-y-1/2 w-28 cursor-pointer z-50" />
+            <img onClick={handlePrev} src='https://pub-c2bb244c4b2641f99eb92df5396cefa1.r2.dev/img/left-arrow.png' alt='prev' className="absolute left-1 top-[67%] -translate-y-1/2 w-28 cursor-pointer z-50 md:left-10 md:top-1/2" />
+            <img onClick={handleNext} src='https://pub-c2bb244c4b2641f99eb92df5396cefa1.r2.dev/img/right-arrow.png' alt='next' className="absolute right-1 top-[67%] -translate-y-1/2 w-28 cursor-pointer z-5 md:right-10 md:top-1/2" />
             <div className="absolute top-10 left-1/2 -translate-x-1/2 font-sans font-bold text-3xl z-50">{products.length > 0 && products[activeItem].name}</div>
-            <div className="absolute bottom-32 left-1/2 -translate-x-1/2 font-sans font-bold text-3xl z-50">{products.length > 0 && products[activeItem].price} AED</div>
-            <div className="absolute bottom-24 left-1/2 -translate-x-1/2 font-sans font-bold text-3xl z-50">{products.length > 0 && products[activeItem].uagb_excerpt}</div>
+            <div className="absolute bottom-28 left-1/2 -translate-x-1/2 z-50 w-full md:w-auto">
+                <div className="font-bold text-3xl text-center mb-2">{products.length > 0 && products[activeItem].price} AED</div>
+                <div className="font-bold text-center text-1">{products.length > 0 && products[activeItem].uagb_excerpt}</div>
+            </div>
             <button
                 onClick={() => handlePreBooking(products[activeItem])}
                 className="absolute bottom-10 left-1/2 -translate-x-1/2 font-sans font-bold text-xl hover:bg-white/40 border-2 border-white rounded-full px-10 py-2 z-50"
@@ -108,7 +122,7 @@ const CollectionPage = ({ show }) => {
                 <ambientLight />
                 <Environment preset="city" />
                 <CameraController active={activeItem} items={items} />
-                <Models items={items} active={activeItem} paused={paused} scaleFactor={scaleFactor} />
+                <Models items={items} active={activeItem} paused={paused} scaleFactor={scaleFactor} productName={products[activeItem]?.name} />
             </Canvas>
         </div>
     )
@@ -118,13 +132,21 @@ CollectionPage.propTypes = {
     show: PropTypes.bool.isRequired,
 };
 
-const Models = ({ items, active, paused, scaleFactor }) => {
+const Models = ({ items, active, paused, scaleFactor, productName }) => {
     const ref = useRef([]);
 
     // Load textures using local image imports
-    const bugTexture = useLoader(TextureLoader, bugImg);
-    const flowerTexture = useLoader(TextureLoader, flowerImg);
-    const ringTexture = useLoader(TextureLoader, ringImg);
+    const FlitzyTexture = useLoader(TextureLoader, Flitzy);
+    const RosellaTexture = useLoader(TextureLoader, Rosella);
+    const OrphéeTexture = useLoader(TextureLoader, Orphée);
+    const ZekeTexture = useLoader(TextureLoader, Zeke);
+    const CallaLillyTexture = useLoader(TextureLoader, CallaLilly);
+    const BellaTexture = useLoader(TextureLoader, Bella);
+    const BuzzyTexture = useLoader(TextureLoader, Buzzy);
+    const OrabellaTexture = useLoader(TextureLoader, Orabella);
+    const AuréliaTexture = useLoader(TextureLoader, Aurélia);
+    const AméthiaTexture = useLoader(TextureLoader, Améthia);
+    const ZiggyTexture = useLoader(TextureLoader, Ziggy);
 
     useFrame(({ pointer }) => {
         if (paused) return;  // Stop animation if paused
@@ -136,26 +158,75 @@ const Models = ({ items, active, paused, scaleFactor }) => {
         <Float speed={5} rotationIntensity={0.05} floatIntensity={1} floatingRange={[0, 0.4]} >
             {
                 items.map((item, idx) => {
-                    const modelIndex = idx % 3;
-                    switch (modelIndex) {
-                        case 0:
+
+                    switch (productName) {
+                        case 'Flitzy':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[60 * scaleFactor, 60 * scaleFactor, 1]} >
-                                    <spriteMaterial attach="material" map={bugTexture} />
+                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 10 * scaleFactor : 10 * scaleFactor, window.innerWidth < 768 ? 10 * scaleFactor : 15 * scaleFactor, 1]} >
+                                    <spriteMaterial attach="material" map={FlitzyTexture} />
                                 </sprite>
                             );
-                        case 1:
+                        case 'Rosella':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[40 * scaleFactor, 45 * scaleFactor, 1]} >
-                                    <spriteMaterial attach="material" map={flowerTexture} />
+                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 15 * scaleFactor : 25 * scaleFactor, window.innerWidth < 768 ? 20 * scaleFactor : 30 * scaleFactor, 1]} >
+                                    <spriteMaterial attach="material" map={RosellaTexture} />
                                 </sprite>
                             );
-                        case 2:
+                        case 'Orphée':
                             return (
-                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[60 * scaleFactor, 60 * scaleFactor, 1]} >
-                                    <spriteMaterial attach="material" map={ringTexture} />
+                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 20 * scaleFactor : 30 * scaleFactor, window.innerWidth < 768 ?  35 * scaleFactor : 45 * scaleFactor, 1]} >
+                                    <spriteMaterial attach="material" map={OrphéeTexture} />
                                 </sprite>
                             );
+                        case 'Zeke':
+                            return (
+                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, 1]} >
+                                    <spriteMaterial attach="material" map={ZekeTexture} />
+                                </sprite>
+                            );
+                        case 'Calla Lilly':
+                            return (
+                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 15 * scaleFactor : 20 * scaleFactor, window.innerWidth < 768 ? 25 * scaleFactor : 25 * scaleFactor, 1]} >
+                                    <spriteMaterial attach="material" map={CallaLillyTexture} />
+                                </sprite>
+                            );
+                        case 'Bella':
+                            return (
+                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, 1]} >
+                                    <spriteMaterial attach="material" map={BellaTexture} />
+                                </sprite>
+                            );
+                        case 'Buzzy':
+                            return (
+                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ?  40 * scaleFactor : 60 * scaleFactor, window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, 1]} >
+                                    <spriteMaterial attach="material" map={BuzzyTexture} />
+                                </sprite>
+                            );
+                        case 'Orabella':
+                            return (
+                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ?  20 * scaleFactor : 25 * scaleFactor, window.innerWidth < 768 ? 25 * scaleFactor : 35 * scaleFactor, 1]} >
+                                    <spriteMaterial attach="material" map={OrabellaTexture} />
+                                </sprite>
+                            );
+                        case 'Aurélia':
+                            return (
+                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 20 * scaleFactor : 30 * scaleFactor, window.innerWidth < 768 ? 30 * scaleFactor : 45 * scaleFactor, 1]} >
+                                    <spriteMaterial attach="material" map={AuréliaTexture} />
+                                </sprite>
+                            );
+                        case 'Améthia':
+                            return (
+                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 15 * scaleFactor : 20 * scaleFactor, window.innerWidth < 768 ? 25 * scaleFactor : 25 * scaleFactor, 1]} >
+                                    <spriteMaterial attach="material" map={AméthiaTexture} />
+                                </sprite>
+                            );
+                        case 'Ziggy':
+                            return (
+                                <sprite ref={el => ref.current[idx] = el} {...item} scale={[window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, window.innerWidth < 768 ? 40 * scaleFactor : 60 * scaleFactor, 1]} >
+                                    <spriteMaterial attach="material" map={ZiggyTexture} />
+                                </sprite>
+                            );
+                        
                         default:
                             return null;
                     }
